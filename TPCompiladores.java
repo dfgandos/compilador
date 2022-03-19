d86e639f722d382f2d58b3ab02708911a40b62db
Ygor Melo <ygorlmelo@gmail.com>
2022-03-18 22:06:50 -0300

    Adicionado enum para alfabeto e classe de criação de simbolo

--- TPCompiladores.java
+++ TPCompiladores.java
@@ -9,13 +9,13 @@
 import java.util.*;
 import java.io.*;
 
-public class TPCompiladores { 
+public class TPCompiladores {
 
-   public static PushbackReader leitura; 
+   public static PushbackReader leitura;
 
-   public static void main(String[] args){
+   public static void main(String[] args) {
 
-      // LEITURA DO TECLADO 
+      // LEITURA DO TECLADO
       leitura = new PushbackReader(new InputStreamReader(System.in));
       try {
 
@@ -24,13 +24,13 @@ public class TPCompiladores {
       } catch (Exception e) {
          String message = e.getMessage();
          System.out.println(message);
-      }  
+      }
    }
 }
 
-/* 
-   CLASSES DE ERROS
-*/
+/*
+ * CLASSES DE ERROS
+ */
 class ErroPersonalizado extends Exception {
    public ErroPersonalizado(String mensagem) {
       super(mensagem);
@@ -59,4 +59,62 @@ class ErroTokenNaoEsperado extends ErroPersonalizado {
    public ErroTokenNaoEsperado(int linhaPrograma, String lexema) {
       super(linhaPrograma + "\ntoken nao esperado [" + lexema + "].");
    }
+}
+
+/*
+ * ENUM REPRESENTA O ALFABETO DA LINGUAGEM COM A SUA NUMERAÇÃO DE TOKENS
+ * 
+ */
+enum AlfabetoEnum {
+
+   CONST("const"), INTEGER("integer"), CHAR("char"),
+   WHILE("while"), IF("if"), REAL("real"), ELSE("else"),
+   AND("and"), OR("or"), NOT("not"), IGUAL("="), IGUAL_IGUAL("=="),
+   ABRE_PARENTESES("("), FECHA_PARENTESES(")"), MENOR("<"),
+   MAIOR(">"), DIFERENTE("!="), MAIOR_IGUAL(">="),
+   MENOR_IGUAL("<="), VIRGULA(","),
+   MAIS("+"), MENOS("-"), MULTIPLICACAO("*"),
+   DIVISAO("/"), PONTO_VIRGULA(";"), BEGIN("begin"),
+   END("end"), READLN("readln"), BARRA_BARRA("//"), STRING("string"),
+   WRITE("write"), WRITELN("writeln"), PORCETAGEM("%"),
+   ABRE_COLCHETE("["), FECHA_COLCHETE("]"),
+   TRUE("TRUE"), FALSE("FALSE"), BOOLEAN("boolean"),
+   IDENTIFICADOR("id"), EOF("eof");
+
+   private String enumAlfabeto;
+
+   AlfabetoEnum(String enumAlfabeto) {
+      this.enumAlfabeto = enumAlfabeto;
+   }
+
+   public String getEnumAlfabeto() {
+      return this.enumAlfabeto;
+   }
+
+}
+
+/*
+ * CLASSE PARA CRIAÇÃO DOS SIMBOLOS
+ */
+class Simbolo {
+
+   private AlfabetoEnum token;
+   private int tamanho;
+
+   Simbolo(AlfabetoEnum token) {
+      this.token = token;
+   }
+
+   public AlfabetoEnum getToken() {
+      return token;
+   }
+
+   public void setTamanho(int tamanho) {
+      this.tamanho = tamanho;
+   }
+
+   public int getTamanho() {
+      return this.tamanho;
+   }
+
 }
